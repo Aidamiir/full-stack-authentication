1bb3f5a10727d2bd7b236b0006439cb28c1dc7c3
Aidamir <kambievv.07@icloud.com>
2024-05-13 17:27:58 +0300

    feat: added recaptcha in sign-in and sign-up pages

--- frontend/src/pages/sign-in/sing-in.tsx
+++ frontend/src/pages/sign-in/sing-in.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReCAPTCHA from 'react-google-recaptcha';
 import { useSignInMutation } from '../../shared/api';
 import { ROUTES } from '../../shared/constants/routes';
 import { useForm, SubmitHandler } from 'react-hook-form';
@@ -8,12 +9,13 @@ import cls from  './sign-in.module.scss';
 
 export const SignInPage = () => {
   const navigation = useNavigate();
+  const [token, setToken] = useState('');
   const [signIn, { isLoading, isError }] = useSignInMutation();
   const { register, reset, handleSubmit, formState: { errors } } = useForm<Credentials>();
 
   const onSubmit: SubmitHandler<Credentials> = async ({ email, password }) => {
     try {
-      await signIn({ email, password }).unwrap();
+      await signIn({ email, password, token }).unwrap();
       navigation(ROUTES.root);
     }
     catch (error) {
@@ -22,6 +24,10 @@ export const SignInPage = () => {
     finally {
       reset();
     }
+  };
+
+  const onChange = (token: any) => {
+    setToken(token);
   };
 
   return (
@@ -37,6 +43,7 @@ export const SignInPage = () => {
             <label className={`${errors.password || isError ? cls.error : '' } ${cls.label}`}>Пароль:</label>
             <input  className={`default-input ${cls.input} ${errors.password || isError ? cls.error : ''}`} type="password" {...register("password", { required: "Введите ваш пароль" })} />
           </div>
+          <ReCAPTCHA className={cls.recaptcha} sitekey="6LcdztopAAAAAP5pG1HofPXWwrEfoFzT6-MLtPkM" onChange={onChange} />
           <div className={cls.buttons}>
             <button className="button" type="submit" disabled={isLoading}>Войти</button>
             <button className="button" type="button" onClick={() => navigation(ROUTES.signUp)} disabled={isLoading}>Зарегистрироваться</button>
--- frontend/src/pages/sign-up/sign-up.tsx
+++ frontend/src/pages/sign-up/sign-up.tsx
@@ -4,15 +4,18 @@ import { ROUTES } from '../../shared/constants/routes';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Credentials } from '../../shared/interfaces/auth.interface';
 import cls from  './sign-up.module.scss';
+import React, { useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export const SignUpPage = () => {
   const navigation = useNavigate();
+  const [token, setToken] = useState('');
   const [signUp, { isLoading, isError }] = useSignUpMutation();
   const { register, reset, handleSubmit, formState: { errors } } = useForm<Credentials>();
 
   const onSubmit: SubmitHandler<Credentials> = async ({ email, password }) => {
     try {
-      await signUp({ email, password }).unwrap();
+      await signUp({ email, password, token }).unwrap();
       navigation(ROUTES.root);
     }
     catch (error) {
@@ -21,6 +24,10 @@ export const SignUpPage = () => {
     finally {
       reset();
     }
+  };
+
+  const onChange = (token: any) => {
+    setToken(token);
   };
 
   return (
@@ -36,6 +43,7 @@ export const SignUpPage = () => {
             <label className={`${errors.email || isError ? cls.error : '' } ${cls.label}`}>Пароль:</label>
             <input className={`default-input ${cls.input} ${errors.email || isError ? cls.error : ''}`} type="password" {...register("password", { required: "Введите ваш пароль" })} />
           </div>
+          <ReCAPTCHA className={cls.recaptcha} sitekey="6LcdztopAAAAAP5pG1HofPXWwrEfoFzT6-MLtPkM" onChange={onChange} />
           <div className={cls.buttons}>
             <button className="button" type="submit" disabled={isLoading}>Зарегистрироваться</button>
             <button className="button" type="button" onClick={() => navigation(ROUTES.signIn)} disabled={isLoading}>Войти</button>
